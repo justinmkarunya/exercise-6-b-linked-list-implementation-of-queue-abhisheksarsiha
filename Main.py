45ab8420168a540e8e8604dcfd1c960ee28a1c55
abhisheksarsiha <109681977+abhisheksarsiha@users.noreply.github.com>
2022-09-14 09:58:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,12 +11,31 @@ class Queue:
 
   def enqueue(self, data) -> None:
     # Write your code here
+    if self.last is None:
+      self.head = Node(data)
+      self.last = self.head
+    else:
+      self.last.next = Node(data)
+      self.last = self.last.next
 
   def dequeue(self) -> None:
     # Write your code here
+    if self.head is None:
+      return None
+    else:
+      val_returned = self.head.data
+      self.head = self.head.next
 
   def status(self) -> None:
     # Write your code here
+    current = self.head
+    status_of_queue = []
+    while (current):
+      status_of_queue.append(current.data)
+      current = current.next
+    for element in status_of_queue:
+      print(element, end="=>")
+    print(None)
 
 
 # Do not change the following code
